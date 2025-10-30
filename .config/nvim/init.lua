cf178bd247cb8dab4fb07190e06729fd8d8abe63
erasromani <ekr6072@nyu.edu>
2025-10-30 11:18:06 -0400

    add cuda indent pattern

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -438,7 +438,7 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "c", "cpp" },
+  pattern = { "c", "cpp", "cu" },
   callback = function()
     vim.opt_local.tabstop = 2
     vim.opt_local.shiftwidth = 2
